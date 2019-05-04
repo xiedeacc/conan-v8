78992499580eccf189d8d225437ca9c7bedc0ffb
a_teammate <madoe3@web.de>
2019-05-04 14:23:57 +0200

    go for the easiest working way to install on linux (shitty build requirements)

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class v8Conan(ConanFile):
     author = "a_teammate <madoe3@web.de>"
     license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
     # exports = ["COPYING"]
-    exports_sources = ["BUILD.gn", "DEPS", "*", "!.git/*"]
+    # exports_sources = ["BUILD.gn", "DEPS", "*", "!.git/*"]
     # exports_sources = ["CMakeLists.txt", "src/*", "!src/*/*/Test", "package/conan/*", "modules/*"]
     generators = "cmake"
     # short_paths = True  # Some folders go out of the 260 chars path length scope (windows)
@@ -59,13 +59,18 @@ class v8Conan(ConanFile):
         """
 
     def build_requirements(self):
+        return
+        # tools.download(
+        '''
         if tools.os_info.is_linux:
             self.build_requires("bison/3.0.4@bincrafters/stable")
             self.build_requires("bzip2/1.0.6@conan/testing")
             self.build_requires("flex/2.6.4@bincrafters/stable")
             self.build_requires("OpenSSL/1.1.0g@conan/stable")
+        '''
 
     def requirements(self):
+        return
 
         if tools.os_info.is_linux:
             self.requires("libuuid/1.0.3@bincrafters/stable")
@@ -74,22 +79,27 @@ class v8Conan(ConanFile):
     def source(self):
         
         # Set up depot_tools
-        # self.run("gclient")
-        # Wrap the original CMake file to call conan_basic_setup
-        shutil.move("CMakeLists.txt", "CMakeListsOriginal.txt")
-        shutil.move(os.path.join("package", "conan", "CMakeLists.txt"), "CMakeLists.txt")
+        self.run("git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git")
+        new_path = "PATH=$PATH:`pwd`/depot_tools "
+        # new_path = ""
+        self.run(new_path + "gclient")
+        self.run(new_path + "fetch v8")
 
 
     def build(self):
-       cmd = "{source_dir}/build/v8/v8/tools/dev/v8gen.py x64.{build_type} -- v8_monolithic=true v8_static_library=true v8_use_external_startup_data=false is_component_build=false is_clang=false use_sysroot=false v8_enable_i18n_support=false v8_enable_backtrace=false use_glib=false use_custom_libcxx=false use_custom_libcxx_for_host = false treat_warnings_as_errors = false && ninja -C out.gn/x64.{build_type} v8_monolith".format(source_dir=, build_type=
-        # NINJA required.
+        new_path = "PATH=$PATH:`pwd`/depot_tools "
+        # new_path = ""
+        if tools.os_info.is_linux:
+          self.run("chmod +x v8/build/install-build-deps.sh")
+          self.run(new_path + "v8/build/install-build-deps.sh --unsupported")
+        cmd = new_path + "cd v8 && tools/dev/v8gen.py x64.{build_type} -- v8_monolithic=true v8_static_library=true v8_use_external_startup_data=false is_component_build=false is_clang=false use_sysroot=false v8_enable_i18n_support=false v8_enable_backtrace=false use_glib=false use_custom_libcxx=false use_custom_libcxx_for_host = false treat_warnings_as_errors = false && ninja -C out.gn/x64.{build_type} v8_monolith".format(build_type=str(self.settings.build_type).lower())
         self.run(cmd)
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="*.a", dst="lib", keep_paths=False)
-        self.copy(pattern="*.lib", dst="lib", keep_paths=False)
-        self.copy(pattern="*.h", dst="include", src="include", keep_paths=True)
+        self.copy(pattern="LICENSE*", dst="licenses", src="v8")
+        self.copy(pattern="*v8_monolith.a", dst="lib", keep_paths=False)
+        self.copy(pattern="*v8_monolith.lib", dst="lib", keep_paths=False)
+        self.copy(pattern="*.h", dst="include", src="v8/include", keep_paths=True)
         
 
     def package_info(self):
